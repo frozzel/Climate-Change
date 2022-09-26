fe9dd2085b30a6975f6a926d3e96155a4a1da4f6
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-09-26 18:32:59 -0400

    changed url to do imperal units & sent info to dom

--- assets/js/scritpt.js
+++ assets/js/scritpt.js
@@ -3,22 +3,21 @@ var city="";
 var searchCity= $('#city-imput');
 var buttonSearch = $('#form-submit')
 var ApiKey= "f94e53623ff698447585ddbe3bf5e63b"
-
+var CurrentTemp= $('#temp');
 ////// checkout api call ////// test in class app by switching out git repo with weather app api calls/////
-  console.log(buttonSearch)
-  console.log(searchCity)
-function test(){
-  var URLtest= "https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=f94e53623ff698447585ddbe3bf5e63b";
-  $.ajax({
-    url:URLtest,
-    method:"GET"
-  }).then(function(response){
-    // console.log(response)
-  }
+ 
+// function test(){
+//   var URLtest= "https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=f94e53623ff698447585ddbe3bf5e63b";
+//   $.ajax({
+//     url:URLtest,
+//     method:"GET"
+//   }).then(function(response){
+//     console.log(response)
+//   }
   
-  );
-}
-  test()
+//   );
+// }
+//   test()
   /// https://openweathermap.org/current ///// api instructions/// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}/// may need ajust var to work with search/////
 
   /////////////// Search funtion ////////////////
@@ -33,10 +32,27 @@ function getsearch(event){
   }
 }
 function getCurrentWeather(city){
+  var UrlApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + ApiKey + "&units=imperial";
+  $.ajax({
+    url:UrlApi,
+    method:"GET"
+  }).then(function(response){
+    console.log(response);
+    var date = new Date(response.dt*1000).toLocaleDateString();
+    $(cityCurrent).html(response.name + "("+date+")");
+    var CurrentTemp = (response.main.temp);
+    console.log(CurrentTemp)
+    console.log(date)
 
 
+
+
+  }
+  
+  );
 }
 
+///////^^^^^look up icon info later for openweather api//////
 
 console.log(city)
   /////////// button////////
