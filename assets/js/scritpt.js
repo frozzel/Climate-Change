76f9f6362d8446b20f8a33124e6bdaf5f2399814
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-09-26 20:19:21 -0400

    done ol next

--- assets/js/scritpt.js
+++ assets/js/scritpt.js
@@ -6,6 +6,8 @@ var ApiKey= "f94e53623ff698447585ddbe3bf5e63b"
 var CurrentTemp= $('#temp');
 var CurrentHumidity = $('#humidity')
 var CurrentWind = $('wind')
+var CurrentIcon =$('#Icon')
+var CurrentDate =$('#date')
 ////// checkout api call ////// test in class app by switching out git repo with weather app api calls/////
  
 // function test(){
@@ -40,23 +42,26 @@ function getCurrentWeather(city){
     method:"GET"
   }).then(function(response){
     console.log(response);
+    var weatherIcon = response.weather[0].icon;
+    console.log(weatherIcon)
+    var UrlIcons= "https://openweathermap.org/img/wn/" + weatherIcon + '@2x.png';
+    console.log(UrlIcons)
     var date = new Date(response.dt*1000).toLocaleDateString();
-    $(cityCurrent).html(response.name + " ("+date+")");
+    $(cityCurrent).html(response.name + "<img src="+UrlIcons+">");
+    $(CurrentDate).html(date)
     $(temp).html("Temperature: " + response.main.temp + "°")
     $(humidity).html("Humidity: "+response.main.humidity+ "%")
     $(wind).html("Wind: " + response.wind.speed + " MPH")
-   console.log(humidity)
-    console.log(date)
-
-
-
-
+   
   }
   
   );
 }
 
-///////^^^^^look up icon info later for openweather api//////
+
+////// https://openweathermap.org/weather-conditions///////
+///////^^^^^look up icon info later for openweather api////// cant get to work////
+
 
 console.log(city)
   /////////// button////////
